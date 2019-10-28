c206804840c24d549fab2048db288ddb43ca78b4
Karma <lamax007@umn.edu>
2019-10-28 01:58:14 -0500

    midterm

--- midterm.js
+++ midterm.js
@@ -1,14 +1,11 @@
-var cres;
-var wres;
+
 var url;
-/*for (var i = 0; i < (gres.results).length; i++) {
-  gres.results[i].name
-}*/
+
 function wsearch(){
   var name = document.querySelector('#name').value;
   name = name.replace(' ', '%20');
   var wurl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search='+name;
-  fetch(wurl,{headers: Access-Control-Allow-Origin: *}
+  fetch(wurl)
   .then(function(response){
     return response.json();
   }).then(function(data){
@@ -21,11 +18,11 @@ function create(src){
   url = src[3][0];
   console.log(url);
   var curl = 'https://api.qrserver.com/v1/create-qr-code/?data=' +url + '&size=100x100';
-  fetch(curl, Access-Control-Allow-Origin: *)
+  fetch(curl)
   .then(function(response){
     var img = document.createElement('img');
     img.src = curl;
     console.log(img);
-    document.body.appendChild(img);
+    document.getElementById('p1').appendChild(img);
   })
 }
