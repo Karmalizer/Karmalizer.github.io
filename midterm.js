6c3dc67f6301785955ae11870d3e70e0134cded4
Karma <lamax007@umn.edu>
2019-10-28 01:21:28 -0500

    midterm

--- midterm.js
+++ midterm.js
@@ -8,23 +8,24 @@ function wsearch(){
   var name = document.querySelector('#name').value;
   name = name.replace(' ', '%20');
   var wurl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search='+name;
-  fetch(wurl, {
-    method: 'GET',
-  }).then(function(response){
-
-    wres = response.json();
-    url = wres[0][3][0];
-    console.log(url);
-    console.log(wres);
-  }).then(create())
+  fetch(wurl)
+  .then(function(response){
+    return response.json();
+  }).then(function(data){
+    console.log(data);
+    create(data);
+  })
 }
 
-function create(){
+function create(src){
+  url = src[3][0];
+  console.log(url);
   var curl = 'https://api.qrserver.com/v1/create-qr-code/?data=' +url + '&size=100x100';
-  fetch(curl,{
-    method: 'GET'
-  }).then(function(response){
-    cres= response;
-    console.log(cres);
+  fetch(curl)
+  .then(function(response){
+    var img = document.createElement('img');
+    img.src = curl;
+    console.log(img);
+    document.body.appendChild(img);
   })
 }
